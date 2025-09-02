f84797b6416e7c26a787ef76ac50fd167c573f06
solnarcisse <solnarcisse@gmail.com>
2025-09-01 23:17:01 -0500

    code refactor, in progress

--- Uhuru/0-authentication.tf
+++
@@ -1,9 +0,0 @@
-
-
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs
-provider "google" {
-  project     = "backendformytf6969"
-  region      = "us-east1"
-  credentials = "key.json"
-}
-
